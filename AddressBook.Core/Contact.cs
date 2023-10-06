70ecf9fe42f91e04ce23502f89eb7d316e57b593
duhankosali <duhanksali@gmail.com>
2023-10-06 12:08:55 +0300

    Core Layer
    Repositories --> Interaction with the database. CRUD operations
    Services --> Business logic operations.
    UnitOfWorks --> It organizes transactions between various repositories. (It was added as an option for this project.)

---
+++ AddressBook.Core/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Core
+{
+    public class Contact : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string? MobilePhone { get; set; }
+        public string? Email { get; set; }   
+    }
+}
---
+++ AddressBook.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace AddressBook.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id); // get by Id
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate); // get all
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate); // search
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity); // add
+        void Update(T entity);  // update
+        void Delete(T entity); // delete
+    }
+}
